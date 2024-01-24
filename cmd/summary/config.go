6e1ab925f9e45857d2c70bc492f0afee5d72cd0b
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-01-24 08:59:47 +0000

    chore(serve): Removing redundant code (#36)
    * Removing redundant code
    
    * lint
    
    * lint
    
    * using setup logging method

--- cmd/summary/config.go
+++ cmd/summary/config.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
 
-	"github.com/Jacobbrewer1/puppet-summary/pkg/dataaccess"
+	"github.com/Jacobbrewer1/puppet-summary/pkg/logging"
 )
 
 const (
@@ -16,28 +15,13 @@ var (
 	uploadToken string
 )
 
-func (s *serveCmd) generateConfig() error {
-	err := dataaccess.ConnectDatabase(s.dbType)
+func setupLogging() error {
+	lCfg := logging.NewConfig(appName)
+
+	_, err := logging.CommonLogger(lCfg)
 	if err != nil {
-		return fmt.Errorf("error connecting to database: %w", err)
+		return fmt.Errorf("error creating common logger: %w", err)
 	}
-	if s.gcs {
-		dataaccess.GCSEnabled = true
-		err = dataaccess.ConnectGCS(s.gcsBucket)
-		if err != nil {
-			return fmt.Errorf("error connecting to GCS: %w", err)
-		}
-	}
-	if s.uploadToken != "" {
-		slog.Info("Upload token set, security on upload endpoint is enabled")
-		uploadToken = s.uploadToken
-	} else {
-		slog.Info("Upload token not set, upload endpoint is not secure")
-	}
-	if s.autoPurge != 0 {
-		slog.Info(fmt.Sprintf("Auto purge set to %d days", s.autoPurge))
-	} else {
-		slog.Info("Auto purge not set, data will not be purged")
-	}
+
 	return nil
 }
