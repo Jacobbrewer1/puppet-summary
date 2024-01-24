94e268a86624bb602464f3122ddcee2f5e09f819
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-01-24 08:50:25 +0000

    Removing redundant code

--- cmd/summary/cmd_serve.go
+++ cmd/summary/cmd_serve.go
@@ -3,10 +3,16 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"github.com/gorilla/mux"
 	"log/slog"
+	"net/http"
 
+	"github.com/Jacobbrewer1/puppet-summary/pkg/dataaccess"
 	"github.com/Jacobbrewer1/puppet-summary/pkg/logging"
+	"github.com/Jacobbrewer1/puppet-summary/pkg/request"
 	"github.com/google/subcommands"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type serveCmd struct {
@@ -48,20 +54,80 @@ func (s *serveCmd) SetFlags(f *flag.FlagSet) {
 	f.StringVar(&s.gcsBucket, "gcs-bucket", "", "The name of the Google Cloud Storage bucket to use. (Only used if gcs is enabled.)")
 }
 
-func (s *serveCmd) Execute(_ context.Context, _ *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	a, err := initializeApp()
-	if err != nil {
-		slog.Error("Error initializing application", slog.String(logging.KeyError, err.Error()))
-		return subcommands.ExitFailure
-	}
+func (s *serveCmd) Execute(ctx context.Context, _ *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	r := mux.NewRouter()
+
 	if err := s.generateConfig(); err != nil {
 		slog.Error("Error generating configuration", slog.String(logging.KeyError, err.Error()))
 		return subcommands.ExitFailure
 	}
+	s.setupRoutes(r)
+
 	slog.Debug("Starting application")
-	if err := a.run(s.autoPurge); err != nil {
-		slog.Error("Error running application", slog.String(logging.KeyError, err.Error()))
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			slog.Error("Error serving requests", slog.String(logging.KeyError, err.Error()))
+		}
+	}()
+
+	<-ctx.Done()
+	slog.Debug("Shutting down application")
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Error("Error shutting down application", slog.String(logging.KeyError, err.Error()))
 		return subcommands.ExitFailure
 	}
+
 	return subcommands.ExitSuccess
+}
+
+func (s *serveCmd) generateConfig() error {
+	err := dataaccess.ConnectDatabase(s.dbType)
+	if err != nil {
+		return fmt.Errorf("error connecting to database: %w", err)
+	}
+	if s.gcs {
+		err = dataaccess.ConnectGCS(s.gcsBucket)
+		if err != nil {
+			return fmt.Errorf("error connecting to GCS: %w", err)
+		}
+		dataaccess.GCSEnabled = true
+	}
+	if s.uploadToken != "" {
+		slog.Info("Upload token set, security on upload endpoint is enabled")
+		uploadToken = s.uploadToken
+	} else {
+		slog.Info("Upload token not set, upload endpoint is not secure")
+	}
+	if s.autoPurge != 0 {
+		slog.Info(fmt.Sprintf("Auto purge set to %d days", s.autoPurge))
+	} else {
+		slog.Info("Auto purge not set, data will not be purged")
+	}
+	return nil
+}
+
+func (s *serveCmd) setupRoutes(r *mux.Router) {
+	r.HandleFunc(pathIndex, middlewareHttp(indexHandler, AuthOptionNone)).Methods(http.MethodGet)
+	r.HandleFunc(pathIndexEnv, middlewareHttp(indexHandler, AuthOptionNone)).Methods(http.MethodGet)
+
+	r.HandleFunc(pathUpload, middlewareHttp(uploadHandler, AuthOptionNone)).Methods(http.MethodPost)
+	r.HandleFunc(pathApiState, middlewareHttp(stateHandler, AuthOptionNone)).Methods(http.MethodGet)
+	r.HandleFunc(pathRadiator, middlewareHttp(radiatorHandler, AuthOptionNone)).Methods(http.MethodGet)
+	r.HandleFunc(pathSearch, middlewareHttp(searchHandler, AuthOptionNone)).Methods(http.MethodPost)
+	r.HandleFunc(pathNodeFqdn, middlewareHttp(nodeFqdnHandler, AuthOptionNone)).Methods(http.MethodGet)
+	r.HandleFunc(pathReportID, middlewareHttp(reportIDHandler, AuthOptionNone)).Methods(http.MethodGet)
+
+	r.HandleFunc(pathMetrics, middlewareHttp(promhttp.Handler().ServeHTTP, AuthOptionInternal)).Methods(http.MethodGet)
+	r.HandleFunc(pathHealth, middlewareHttp(healthHandler(), AuthOptionInternal)).Methods(http.MethodGet)
+
+	r.NotFoundHandler = request.NotFoundHandler()
+	r.MethodNotAllowedHandler = request.MethodNotAllowedHandler()
+
+	r.PathPrefix(pathAssets).Handler(http.StripPrefix(pathAssets, http.FileServer(http.Dir("./assets"))))
 }
