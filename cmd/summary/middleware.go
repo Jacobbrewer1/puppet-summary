eab4d640b2bf16976ff10014a5ce9c2494f3f006
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-02-15 13:04:15 +0000

    fix(metrics): Changing how the metrics are reported (#66)
    * Changing how the metrics are reported
    
    * reverting paths

--- cmd/summary/middleware.go
+++ cmd/summary/middleware.go
@@ -72,13 +72,6 @@ func middlewareHttp(handler Controller, authOption AuthOption) http.HandlerFunc 
 			path = r.URL.Path // If the route is nil, use the URL path.
 		}
 
-		defer func() {
-			// Run the deferred function after the request has been handled, as the status code will not be available until then.
-			httpTotalRequests.WithLabelValues(path, r.Method, fmt.Sprintf("%d", cw.StatusCode())).Inc()
-			httpRequestDuration.WithLabelValues(path, r.Method, fmt.Sprintf("%d", cw.StatusCode())).Observe(time.Since(now).Seconds())
-			httpRequestSize.WithLabelValues(path, r.Method, fmt.Sprintf("%d", cw.StatusCode())).Observe(float64(r.ContentLength))
-		}()
-
 		switch authOption {
 		case AuthOptionNone, AuthOptionInternal, AuthOptionRequired:
 			// Do nothing.
@@ -91,5 +84,10 @@ func middlewareHttp(handler Controller, authOption AuthOption) http.HandlerFunc 
 		}
 
 		handler(cw, r)
+
+		// Record the request metrics.
+		httpTotalRequests.WithLabelValues(path, r.Method, fmt.Sprintf("%d", cw.StatusCode())).Inc()
+		httpRequestDuration.WithLabelValues(path, r.Method, fmt.Sprintf("%d", cw.StatusCode())).Observe(time.Since(now).Seconds())
+		httpRequestSize.WithLabelValues(path, r.Method, fmt.Sprintf("%d", cw.StatusCode())).Observe(float64(r.ContentLength))
 	}
 }
