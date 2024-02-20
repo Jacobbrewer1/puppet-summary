345de5d58de174ac88572a8fae4ec2e1d55eb206
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-02-20 19:39:06 +0000

    Refactoring db mock name

--- pkg/dataaccess/db_mock.go
+++ pkg/dataaccess/db_mock.go
@@ -8,56 +8,56 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type DbMock struct {
+type MockDb struct {
 	mock.Mock
 }
 
-func (d *DbMock) Ping(ctx context.Context) error {
-	args := d.Called(ctx)
+func (m *MockDb) Ping(ctx context.Context) error {
+	args := m.Called(ctx)
 	return args.Error(0)
 }
 
-func (d *DbMock) Close(ctx context.Context) error {
-	args := d.Called(ctx)
+func (m *MockDb) Close(ctx context.Context) error {
+	args := m.Called(ctx)
 	return args.Error(0)
 }
 
-func (d *DbMock) SaveRun(ctx context.Context, run *entities.PuppetReport) error {
-	args := d.Called(ctx, run)
+func (m *MockDb) SaveRun(ctx context.Context, run *entities.PuppetReport) error {
+	args := m.Called(ctx, run)
 	return args.Error(0)
 }
 
-func (d *DbMock) GetRuns(ctx context.Context) ([]*entities.PuppetRun, error) {
-	args := d.Called(ctx)
+func (m *MockDb) GetRuns(ctx context.Context) ([]*entities.PuppetRun, error) {
+	args := m.Called(ctx)
 	return args.Get(0).([]*entities.PuppetRun), args.Error(1)
 }
 
-func (d *DbMock) GetRunsByState(ctx context.Context, state entities.State) ([]*entities.PuppetRun, error) {
-	args := d.Called(ctx, state)
+func (m *MockDb) GetRunsByState(ctx context.Context, state entities.State) ([]*entities.PuppetRun, error) {
+	args := m.Called(ctx, state)
 	return args.Get(0).([]*entities.PuppetRun), args.Error(1)
 }
 
-func (d *DbMock) GetReports(ctx context.Context, fqdn string) ([]*entities.PuppetReportSummary, error) {
-	args := d.Called(ctx, fqdn)
+func (m *MockDb) GetReports(ctx context.Context, fqdn string) ([]*entities.PuppetReportSummary, error) {
+	args := m.Called(ctx, fqdn)
 	return args.Get(0).([]*entities.PuppetReportSummary), args.Error(1)
 }
 
-func (d *DbMock) GetReport(ctx context.Context, id string) (*entities.PuppetReport, error) {
-	args := d.Called(ctx, id)
+func (m *MockDb) GetReport(ctx context.Context, id string) (*entities.PuppetReport, error) {
+	args := m.Called(ctx, id)
 	return args.Get(0).(*entities.PuppetReport), args.Error(1)
 }
 
-func (d *DbMock) GetHistory(ctx context.Context, environment entities.Environment) ([]*entities.PuppetHistory, error) {
-	args := d.Called(ctx, environment)
+func (m *MockDb) GetHistory(ctx context.Context, environment entities.Environment) ([]*entities.PuppetHistory, error) {
+	args := m.Called(ctx, environment)
 	return args.Get(0).([]*entities.PuppetHistory), args.Error(1)
 }
 
-func (d *DbMock) GetEnvironments(ctx context.Context) ([]entities.Environment, error) {
-	args := d.Called(ctx)
+func (m *MockDb) GetEnvironments(ctx context.Context) ([]entities.Environment, error) {
+	args := m.Called(ctx)
 	return args.Get(0).([]entities.Environment), args.Error(1)
 }
 
-func (d *DbMock) Purge(ctx context.Context, from time.Time) (int, error) {
-	args := d.Called(ctx, from)
+func (m *MockDb) Purge(ctx context.Context, from time.Time) (int, error) {
+	args := m.Called(ctx, from)
 	return args.Int(0), args.Error(1)
 }
