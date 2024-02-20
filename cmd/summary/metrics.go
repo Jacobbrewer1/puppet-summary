4d6a314f5d2f4ef95c31761358ec77bb4d1e3bb5
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-02-20 12:08:50 +0000

    Adding the metrics to a namespace rather than formatting with the app name

--- cmd/summary/metrics.go
+++ cmd/summary/metrics.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
@@ -11,8 +9,9 @@ var (
 	// httpTotalRequests is the total number of http requests.
 	httpTotalRequests = promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: fmt.Sprintf("%s_http_total_requests", appName),
-			Help: "Total number of http requests",
+			Name:      "http_requests_total",
+			Namespace: appName,
+			Help:      "Total number of http requests",
 		},
 		[]string{"path", "method", "status_code"},
 	)
@@ -20,8 +19,9 @@ var (
 	// httpRequestDuration is the duration of the http request.
 	httpRequestDuration = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name: fmt.Sprintf("%s_http_request_duration", appName),
-			Help: "Duration of the http request",
+			Name:      "http_request_duration_seconds",
+			Namespace: appName,
+			Help:      "Duration of the http request",
 		},
 		[]string{"path", "method", "status_code"},
 	)
@@ -29,8 +29,9 @@ var (
 	// httpRequestSize is the size of the http request.
 	httpRequestSize = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name: fmt.Sprintf("%s_http_request_size", appName),
-			Help: "Size of the http request",
+			Name:      "http_request_size",
+			Namespace: appName,
+			Help:      "Size of the http request",
 		},
 		[]string{"path", "method", "status_code"},
 	)
