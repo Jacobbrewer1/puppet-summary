3be1b075832f705302b3a91858751ae2ff892ae9
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-02-29 16:29:11 +0000

    correcting tests

--- cmd/summary/index_test.go
+++ cmd/summary/index_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type indexSuite struct {
+type GetAllNodesSuite struct {
 	suite.Suite
 
 	// db is the database used for testing.
@@ -19,20 +19,20 @@ type indexSuite struct {
 }
 
 func TestIndexSuite(t *testing.T) {
-	suite.Run(t, new(indexSuite))
+	suite.Run(t, new(GetAllNodesSuite))
 }
 
-func (s *indexSuite) SetupTest() {
+func (s *GetAllNodesSuite) SetupTest() {
 	s.db = new(dataaccess.MockDb)
 	dataaccess.DB = s.db
 }
 
-func (s *indexSuite) TearDownTest() {
+func (s *GetAllNodesSuite) TearDownTest() {
 	s.db.AssertExpectations(s.T())
 	s.db = nil
 }
 
-func (s *indexSuite) TestIndexHandlerAPI() {
+func (s *GetAllNodesSuite) TestIndexHandlerAPI() {
 	m := s.db
 
 	now := time.Now().UTC()
@@ -66,12 +66,14 @@ func (s *indexSuite) TestIndexHandlerAPI() {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/nodes", nil)
 
-	indexHandler(w, r)
+	svc := new(webService)
+
+	svc.GetAllNodes(w, r)
 
 	s.Equal(200, w.Code)
 	s.Equal("application/json", w.Header().Get("Content-Type"))
 
 	// Compare the response.
-	expected := "[{\"id\":\"\",\"fqdn\":\"test1\",\"env\":\"PRODUCTION\",\"state\":\"\",\"exec_time\":\"" + now.Format(time.RFC3339) + "\",\"runtime\":\"10s\"},{\"id\":\"\",\"fqdn\":\"test2\",\"env\":\"STAGING\",\"state\":\"\",\"exec_time\":\"" + now.Format(time.RFC3339) + "\",\"runtime\":\"10s\"},{\"id\":\"\",\"fqdn\":\"test3\",\"env\":\"PRODUCTION\",\"state\":\"\",\"exec_time\":\"" + now.Format(time.RFC3339) + "\",\"runtime\":\"10s\"}]\n"
+	expected := "[{\"fqdn\":\"test1\",\"env\":\"PRODUCTION\",\"state\":\"\",\"exec_time\":\"" + now.Format(time.RFC3339) + "\",\"runtime\":\"10s\"},{\"fqdn\":\"test2\",\"env\":\"STAGING\",\"state\":\"\",\"exec_time\":\"" + now.Format(time.RFC3339) + "\",\"runtime\":\"10s\"},{\"fqdn\":\"test3\",\"env\":\"PRODUCTION\",\"state\":\"\",\"exec_time\":\"" + now.Format(time.RFC3339) + "\",\"runtime\":\"10s\"}]\n"
 	s.Require().Equal(expected, w.Body.String())
 }
