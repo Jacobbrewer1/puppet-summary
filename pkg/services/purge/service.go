651456a9fa1828d9202c8e0f69e0909b3a8f0399
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-03-02 16:48:13 +0000

    Vendor conflicts

---
+++ pkg/services/purge/service.go
@@ -0,0 +1,21 @@
+package purge
+
+import (
+	"github.com/Jacobbrewer1/puppet-summary/pkg/dataaccess"
+)
+
+type Purger interface {
+	PurgeData(purgeDays int) error
+
+	SetupPurge(purgeDays int) error
+}
+
+type service struct {
+	db dataaccess.Database
+}
+
+func NewService(db dataaccess.Database) Purger {
+	return &service{
+		db: db,
+	}
+}
